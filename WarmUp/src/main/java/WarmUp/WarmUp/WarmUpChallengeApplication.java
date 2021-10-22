7037d64f1e95f4ad1316a3d07c002f8530d6e8ad
alexB0999 <alexbahi.99@gmail.com>
2021-10-22 08:46:12 -0300

    Initial Commit

---
+++ WarmUp/src/main/java/WarmUp/WarmUp/WarmUpChallengeApplication.java
@@ -0,0 +1,13 @@
+package WarmUp.WarmUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmUpChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmUpChallengeApplication.class, args);
+	}
+
+}
---
+++ WarmUp/src/test/java/WarmUp/WarmUp/WarmUpChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package WarmUp.WarmUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarmUpChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
